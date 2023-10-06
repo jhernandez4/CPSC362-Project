b382bb67ddb24636fb6afaf2120fc54c17f51b92
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-10-06 16:30:11 -0700

    Use login page as a template for sign up

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # URLs defined here
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template('login.html', text="Welcome to your daily activity reminder!!")
 
 @auth.route('/logout')
@@ -12,7 +13,7 @@ def logout():
     #return "<p>logout</p>"
     return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template('sign_up.html')
 
