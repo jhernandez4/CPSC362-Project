85f7573d53c47eeec57be48fbc962b98a9091d68
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-10-13 16:52:08 -0700

    Fix column name typo

--- website/models.py
+++ website/models.py
@@ -11,6 +11,6 @@ class Note(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    pasword = db.Column(db.String(150))
+    password = db.Column(db.String(150))
     username = db.Column(db.String(150))
     notes = db.relationship('Note')
