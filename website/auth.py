0d1978a61877368a21bc5b60fd503df1cdb61f0c
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-10-13 16:20:28 -0700

    Create user input verification for sign up page

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-
+from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 # URLs defined here
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        return redirect(url_for('views.home'))
-    return render_template('login.html', text="Welcome to your daily activity reminder!!")
+    return render_template('login.html', text="Welcome to your daily activity reminder!")
 
 @auth.route('/logout')
 def logout():
@@ -16,5 +15,23 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(username) < 3:
+            flash('Username must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords must match', category='error')
+        elif len(password1) < 5:
+            flash('Password must be at least 5 characters.', category='error')
+        else:
+            # new_user = User(email=email,)
+            flash('Account created!', category='success')
+            
     return render_template('sign_up.html')
 
