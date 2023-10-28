ccaef5a70840b14adb3fb39b83b3fbb176e19b76
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-10-27 19:46:16 -0700

    Add flask_login modules

--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template,session
-
+from flask_login import login_required, current_user
 # URLs defined here
 views = Blueprint('views', __name__)
 
 #Runs when we go to / homepage
 @views.route('/')
+@login_required
 def home():
     username = session.get('username')
-    return render_template("home.html",username=username)
+    return render_template("home.html",user=current_user)
 
