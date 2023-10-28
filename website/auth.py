eabc47f3c89e6485721f49895e396e6cb8b029f9
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-10-27 19:45:31 -0700

    Add flask_login moduls to login and logout users

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 # URLs defined here
 auth = Blueprint('auth', __name__)
@@ -19,18 +20,21 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 session['username'] = user.username
+                login_user(user, remember=True)
                 flash('Logged in successfully!', category='success')
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password!', category='error')
         else:
             flash('An account with this username/email and password combination does not exist', category='error')
-    return render_template('login.html', text="Welcome to your daily activity reminder!")
+    return render_template('login.html', text="Welcome to your daily activity reminder!", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
     #return "<p>logout</p>"
-    return render_template('logout.html')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -59,10 +63,11 @@ def sign_up():
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'), name=username )
+            return redirect(url_for('views.home') )
             
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
 
 def has_at(email):
     for letter in email:
