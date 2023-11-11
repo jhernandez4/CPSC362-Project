9decb97e7452a27441b8db94dc8167f9a0f43bfb
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-11-10 22:04:49 -0800

    Create addNote function
    The addNote js function sends the data inputted to the textarea
    
    The add-note endpoint updates the database and returns the noteId
    
    Finally, addNote takes in noteId and responds by updating the UI

--- website/static/index.js
+++ website/static/index.js
@@ -15,3 +15,46 @@ function deleteNote(noteId) {
         console.error('Error deleting note:', error);
     })
 }
+
+function addNote(){
+    let noteData = document.getElementById('note').value;
+
+    if (noteData != ''){
+    fetch("/add-note" , {
+        method: "POST",
+        body: JSON.stringify({noteData: noteData}),
+    }).then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        else{
+            console.error('Failed to add note:', response.status, response.statusText);
+        }
+    }).then(data => {
+        let newNoteId = data.noteId;
+
+        let newNoteElement = document.createElement('li');
+        newNoteElement.className = 'list-group-item note';
+        newNoteElement.id = newNoteId;
+        newNoteElement.innerHTML = noteData;
+
+        // Add a delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.type = 'button';
+        deleteButton.className = 'close';
+        deleteButton.innerHTML = '<span aria-hidden="true">&times;</span>';
+        deleteButton.onclick = function() {
+            deleteNote(newNoteId);
+        };
+
+        newNoteElement.appendChild(deleteButton);
+
+        // Add the new note to the existing list
+        document.getElementById('notes').appendChild(newNoteElement);
+
+        document.getElementById('note').value = "";
+    }).catch(error => {
+        console.error('Error adding note:', error);
+    });
+}
+}
