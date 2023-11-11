a0867ac2b0e32c9314d90ffa15c46e34e723a342
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-11-10 20:41:13 -0800

    Adjust html when user deletes a note
    Each note is identified and then deleted in the html instead of reloading

--- website/static/index.js
+++ website/static/index.js
@@ -2,7 +2,16 @@ function deleteNote(noteId) {
     fetch("/delete-note", {
         method: "POST",
         body: JSON.stringify({ noteId: noteId}),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
+    }).then(response => {
+        if (response.ok) {
+            // Note successfully deleted, update the UI accordingly
+            document.getElementById(noteId).remove();
+
+        } else {
+            // Handle error cases if needed
+            console.error('Failed to delete note:', response.status, response.statusText);
+        }
+    }).catch(error => {
+        console.error('Error deleting note:', error);
+    })
 }
