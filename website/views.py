2b793d4c74dd45817553cb5899f16bd5d3c2e54b
Joksan Hernandez <jokhernandez120744@gmail.com>
2023-11-10 21:43:37 -0800

    Create add_note endpoint
    The endpoint adds the note to the database without re-rendering the page

--- website/views.py
+++ website/views.py
@@ -11,19 +11,32 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    # username = session.get('username')
-    if request.method == 'POST':
-        note = request.form.get('note')
+    # # username = session.get('username')
+    # if request.method == 'POST':
+    #     note = request.form.get('note')
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
+    #     if len(note) < 1:
+    #         flash('Note is too short!', category='error')
+    #     else:
+    #         new_note = Note(data=note, user_id=current_user.id)
+    #         db.session.add(new_note)
+    #         db.session.commit()
+    #         flash('Note added!', category='success')
 
     return render_template("home.html",user=current_user)
+
+@views.route('/add-note', methods=['POST'])
+def add_note():
+    data = json.loads(request.data)
+    new_note_data = data.get('noteData')
+    user_id = current_user.id
+
+    new_note = Note(data=new_note_data, user_id=current_user.id)
+    db.session.add(new_note)
+    db.session.commit()
+
+    return jsonify({'noteId': new_note.id})
+
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
